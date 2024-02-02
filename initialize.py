f2d5560e4d0ff3bc9811209be5c6ed0d60875ac5
sle <1985663041@qq.com>
2024-02-02 13:39:17 +0800

    submit small files

---
+++ initialize.py
@@ -0,0 +1,74 @@
+import os  
+import io  
+
+def split_file(big_file_path, small_file_pathes_path, chunk_size=1024*1024*20):  
+    # 打开大文件  
+    with open(big_file_path, 'rb') as f:  
+        # 读取数据块  
+        chunk = f.read(chunk_size)  
+        # 创建临时文件名列表  
+        temp_files = []  
+        i = 1  
+        while chunk:  
+            # 创建临时文件并写入数据块  
+            temp_file = f"{os.path.splitext(big_file_path)[0]}_{i}.tmp"  
+            with open(temp_file, 'wb') as temp:  
+                temp.write(chunk)  
+            # 将临时文件名添加到列表中  
+            temp_files.append(temp_file)  
+            # 读取下一个数据块  
+            chunk = f.read(chunk_size)  
+            i += 1  
+    
+    # 存储临时文件名列表  
+    with open(small_file_pathes_path, 'w', encoding='utf-8') as wfd :
+        str_out = ""
+        for path in temp_files:
+            str_out += path + '\t'
+            
+        wfd.write(str_out[0:-1])
+    
+    # 删除大文件
+    os.remove(big_file_path)
+
+def merge_files(file_paths_path, output_path):  
+    with open(file_paths_path, 'r', encoding='utf-8') as rfd:
+        file_paths = rfd.readlines()[0].split('\t')
+    
+    # 打开输出文件  
+    with open(output_path, 'wb') as out_file:  
+        # 遍历所有小文件  
+        for file_path in file_paths:  
+            # 打开小文件  
+            with open(file_path, 'rb') as in_file:  
+                # 读取数据块并写入输出文件  
+                chunk = in_file.read(4096)  
+                while chunk:  
+                    out_file.write(chunk)  
+                    chunk = in_file.read(4096)  
+    
+    # 删除临时小文件
+    for path in file_paths:
+        os.remove(path)
+    
+    os.remove(file_paths_path) 
+    
+if __name__ == "__main__":
+    # Part A: 切割、合并model_after_fine_turning\model_after_fine_turing.pt大文件
+    # Part A: 01 这里我上传前已经执行完成，你直接运行执行下面的代码即可
+    #split_file ("model_after_fine_turning\model_after_fine_turing.pt", "model_after_fine_turning\pathes.txt")
+    #print("done")
+    
+    # Part A: 02 你直接运行执行这里的代码即可
+    merge_files('model_after_fine_turning\pathes.txt', "model_after_fine_turning\model_after_fine_turing.pt")
+    print("done")
+    
+    
+    # Part B: 切割、合并model\pytorch_model.bin大文件
+    # Part B: 01 这里我上传前已经执行完成，你直接运行执行下面的代码即可
+    #split_file ("model\pytorch_model.bin", "model\pathes.txt")
+    #print("done")
+    
+    # Part B: 02 你直接运行执行这里的代码即可
+    merge_files('model\pathes.txt', "model\pytorch_model.bin")
+    print("done")
---
+++ load_model_after_fine_turning.py
@@ -0,0 +1,127 @@
+# to import pre-trained model 
+import torch
+import torch.nn as nn
+import numpy as np
+from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+from tqdm import tqdm 
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+class ModelLoader(nn.Module):
+    def __init__(self, modelName):
+        super(ModelLoader, self).__init__()  # AttributeError: cannot assign module before Module.__init__() call
+        self.modelName = modelName
+        if self.modelName == "SentenceBert":
+            self.tokenizer= AutoTokenizer.from_pretrained('E_Post2Github/model')     # Note: if you wanna Load model from HuggingFace Hub, set the value of argument as following:
+            self.model = AutoModel.from_pretrained('E_Post2Github/model')            # 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2'
+        elif self.modelName == "SimCSE":                                                                                           
+            self.tokenizer = AutoTokenizer.from_pretrained('IDEA-CCNL/Erlangshen-SimCSE-110M-Chinese')      # Load model from HuggingFace Hub
+            self.model =AutoModelForMaskedLM.from_pretrained('IDEA-CCNL/Erlangshen-SimCSE-110M-Chinese')
+
+        else :
+            raise Exception("only two type model are supported , they are SentenceBert and SimCSE, try SentenceBert or SimCSE modelName")
+    
+    def cosineScore(self, rowTensorA, rowtensorB):  # row vector only one dimention
+        Avec = rowTensorA.detach().numpy().squeeze()
+        Bvec = rowtensorB.detach().numpy().squeeze()
+        similarity_socre = Avec.dot( Bvec ) / ( np.linalg.norm(Avec) * np.linalg.norm(Bvec))
+        return similarity_socre
+    
+    def getSentenceEmbeddingsTuple(self, sentenceTuples):  # (sentenceId, sentence) as a tuple
+        SentenceEmbeddings = []
+        for sentenceTuple in sentenceTuples:  # sentenceTuple include its id and sentence
+            SentenceEmbeddings.append( (sentenceTuple[0], self.getSentenceEmbedding(sentenceTuple[1])) )
+        
+        return  SentenceEmbeddings
+         
+    def getSentenceEmbeddings(self, sentences):  # (sentenceId, sentence) as a tuple
+        SentenceEmbeddings = []
+        for sentence in sentences:  # sentenceTuple include its id and sentence
+            SentenceEmbeddings.append( self.getSentenceEmbedding(sentence) )
+        
+        return  SentenceEmbeddings
+         
+    def getSentenceEmbeddingsMeanpooling(self, sentences):  # (sentenceId, sentence) as a tuple
+        SentenceEmbeddings = []
+        for sentence in sentences:  # sentenceTuple include its id and sentence
+            SentenceEmbeddings.append( self.getSentenceEmbedding(sentence) )
+        
+        row_wise_sum = sum(SentenceEmbeddings)  # row-wise sum 
+
+        # divided by row-vector nums
+        Meanpooling = torch.div(row_wise_sum, len(SentenceEmbeddings))
+        return  Meanpooling
+     
+    def getSentenceEmbedding(self, sentence):
+        if self.modelName == "SentenceBert":
+            return  self.get_sentence_embedding_SentenceBert(sentence)
+        else:
+            return  self.get_sentence_embedding_SimCSE(sentence)
+    
+    
+    def get_sentence_embedding_SentenceBert(self, one_sentence):
+        #Mean Pooling - Take attention mask into account for correct averaging
+        def mean_pooling(model_output, attention_mask):
+            token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+            input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+            return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+        # Tokenize sentences
+        encoded_input = self.tokenizer(one_sentence, padding=True, truncation=True, return_tensors='pt')
+
+        # Compute token embeddings
+        with torch.no_grad():
+            model_output = self.model(**encoded_input)
+
+        # Perform pooling. In this case, max pooling.
+        sentence_embedding = mean_pooling(model_output, encoded_input['attention_mask'])
+        return sentence_embedding 
+
+    def get_sentence_embedding_SentenceBert_Has_Grad(self, one_sentence):
+        #Mean Pooling - Take attention mask into account for correct averaging
+        def mean_pooling(model_output, attention_mask):
+            token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+            input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+            return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+        # Tokenize sentences
+        encoded_input = self.tokenizer(one_sentence, padding=True, truncation=True, return_tensors='pt')
+
+        # Compute token embeddings
+        #with torch.no_grad():
+        model_output = self.model(**encoded_input)
+
+        # Perform pooling. In this case, max pooling.
+        sentence_embedding = mean_pooling(model_output, encoded_input['attention_mask'])
+        return sentence_embedding 
+    
+    
+    def get_sentence_embedding_SimCSE(self, one_sentence):  # one_sentence like "我们去散步，天气不错" and so on
+        inputs=self.tokenizer(one_sentence, return_tensors="pt")
+        outputs = self.model(**inputs, output_hidden_states=True)
+        sentence_tokens_embedding = outputs.hidden_states[-1][:,1:-1,:].squeeze() # BertBase共有12个encoder layer，每层encoder layer的输出都保存在隐状态中了
+        sentence_embedding=torch.mean(sentence_tokens_embedding, dim=0) # 平均池化的句子向量
+        return sentence_embedding
+
+
+class QAEncoderModel(nn.Module):
+    """ Query and Answer Dual Encoder Model with pre-trained Model Sentence-Bert """
+    def __init__(self):
+        super(QAEncoderModel, self).__init__() 
+        self.Encoder = ModelLoader("SentenceBert")
+
+    def forward(self, querys, answers):
+        query_embedding = self.Encoder.get_sentence_embedding_SentenceBert_Has_Grad(querys) 
+        answer_embedding = self.Encoder.get_sentence_embedding_SentenceBert_Has_Grad(answers)
+        return query_embedding, answer_embedding 
+
+
+def get_sentences_embedding(sentences = ["completeness cheak", "过程设计"]):
+    model = torch.load(r"model_after_fine_turning/model_after_fine_turing.pt")
+    model.eval()
+    sentences_embedding = model.Encoder.get_sentence_embedding_SentenceBert(sentences) 
+    print(sentences_embedding)
+
+if __name__ == "__main__" : 
+    get_sentences_embedding()
